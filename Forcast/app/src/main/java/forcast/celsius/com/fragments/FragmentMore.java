74f8d518ea7c4958c47339f1b419088327ab1b0e
Dennis Shar <dennisshar@Denniss-MacBook-Pro.local>
2018-04-28 19:44:49 +0300

    ui changes

--- Forcast/app/src/main/java/forcast/celsius/com/fragments/FragmentMore.java
+++ Forcast/app/src/main/java/forcast/celsius/com/fragments/FragmentMore.java
@@ -4,5 +4,5 @@ package forcast.celsius.com.fragments;
  * Created by dennisshar on 13/01/2018.
  */
 
-public class FragmentSixTeenDays extends BaseFragment {
+public class FragmentMore extends BaseFragment {
 }
